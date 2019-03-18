ac5f8d0bab9b0d6d2fbd915a944a8f57bff9ce63
Reet <reetpragya@gmail.com>
2019-03-18 00:04:48 -0400

    examples with graphics and http

--- dup2.go
+++ dup2.go
@@ -7,5 +7,33 @@ import (
 )
 
 func main() {
-	
+	files := os.Args[1:]
+	counts := make(map[string]int)
+	if(len(files) == 0) {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			// Error checking
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f* os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
 }
--- testfile.py
+++ testfile.py
@@ -1,6 +1,7 @@
-f = open('input.txt', 'w')
+for i in range(4):
+    f = open('input' + str(i) + '.txt', 'w')
 
-for x in range(1, 11):
-    f.write('{0:2d} {1:3d} {2:4d}\n'.format(x, x**2, x**3) * x)
+    for x in range(1*(i + 1), 11*(i + 1)):
+        f.write('{0:2d} {1:3d} {2:4d}\n'.format(x, x**2, x**3) * x)
 
-f.close()
+    f.close()
